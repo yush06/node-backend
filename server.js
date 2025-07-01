395a00ebd43414eec1cd957365062496075cc612
yush06 <ayushsaxena0612@gmail.com>
2025-07-01 23:46:52 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express(); // ✅ app is defined here
+
+const PORT = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.send("Hello, this is your backend speaking!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
